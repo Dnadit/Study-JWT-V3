d5379d124e9e54217867149805bdb52190a41a24
Dnadit <yuhyun.kang07@gmail.com>
2023-05-10 22:52:03 +0900

    finished

---
+++ src/main/java/dnadit/studyJWTv3/domain/LoginReqeust.java
@@ -0,0 +1,12 @@
+package dnadit.studyJWTv3.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+public class LoginReqeust {
+    private String userName;
+}
