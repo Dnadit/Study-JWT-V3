4cc31a3d996b82f98e042860cb75465b90a01610
Dnadit <yuhyun.kang07@gmail.com>
2023-05-10 18:43:05 +0900

    init

---
+++ src/main/java/dnadit/studyJWTv3/StudyJwtV3Application.java
@@ -0,0 +1,13 @@
+package dnadit.studyJWTv3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyJwtV3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudyJwtV3Application.class, args);
+	}
+
+}
---
+++ src/test/java/dnadit/studyJWTv3/StudyJwtV3ApplicationTests.java
@@ -0,0 +1,13 @@
+package dnadit.studyJWTv3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyJwtV3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
